14113eccb8460cc1a2dd5ab37e2c49d02f43260d
Davy <machadodavi3778@gmail.com>
2024-11-08 12:49:44 +0000

    Add files via upload

---
+++ Program22.cs
@@ -0,0 +1,17 @@
+﻿using System;
+ public class n44
+{
+    public static void Main(string[] args)
+    {
+        //exercicio1
+        for (int i = 0; i > -1; i++)
+        {
+            
+            if(i %2== 0 )
+            {
+                Console.WriteLine(i);
+            }
+        }       
+    }
+ }
+
